9a15fd50db7a1ae8b1e2c092cf986f857ba4fea3
Taisekwa <72709327+Taisekwa@users.noreply.github.com>
2022-07-04 11:59:21 +1200

    Create UI.R
    Word predictor app

---
+++ UI.R
@@ -0,0 +1,66 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+ 
+library(shiny)
+
+shinyUI(
+  fixedPage(
+    column(
+      width = 10,
+      offset = 1,
+      titlePanel("Word Prediction"),
+      wellPanel("Auto-completion is a common function on mobile devices.
+                      As a user types, an auto-completion function presents that
+                      user with possible completions to the current word being
+                      typed or probable words that could follow the current word
+                      or phrase after it is typed. The package
+                      \"wordprediction\" provides the latter function."),
+      fixedRow(
+        sidebarPanel(
+          span(
+            textInput(
+              "phrase",
+              "Text Input: (enter all but the last word of a phrase)",
+              value = ""
+            ),
+            actionButton("predictButton", "Predict")
+          )
+        ),
+        mainPanel(
+          strong("Text input predicted:"),
+          textOutput("phrase"),
+          strong("Prediction:"),
+          textOutput("word")
+        )
+      ),
+      tabsetPanel(
+        tabPanel(
+          "Report",
+          br()
+          #includeHTML("report.html")
+        ),
+        tabPanel(
+          "wordprediction Package",
+          br()
+          #includeHTML("wordprediction.html")
+        ),
+        tabPanel(
+          "ui.R",
+          br()
+          #includeHTML("ui.html")
+        ),
+        tabPanel(
+          "server.R",
+          br()
+          #includeHTML("server.html")
+        )
+      )
+    )
+  )
+)
